817650aa0e0a65fc1fda9b9191d3384414177875
AndersonDavi <andesondavid17@gmail.com>
2024-06-12 15:01:59 -0500

    feat: Template formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,24 @@
 import { CommonModule } from '@angular/common';
+
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
   imports: [
     CommonModule,
+    ButtonModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    PasswordModule,
+    RouterLink,
   ],
   templateUrl: './register-page.component.html',
   styles: `
@@ -15,4 +28,4 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {}
