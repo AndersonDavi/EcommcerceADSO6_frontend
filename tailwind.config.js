c3738da6852b5ec82d86af3bc675576c2385bbf2
AndersonDavi <andesondavid17@gmail.com>
2024-06-13 09:53:32 -0500

    fix: correcion estilos botones y variables colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors: {
         primary: "#33CC99",
+        primaryBold: "#2dc290",
+        primaryThin: "#35e4a9",
+        primaryLight: "rgba(53, 228, 169, 0.1)",
         light: "#F6F6F6",
+        danger: "#b11010",
       },
     },
   },
